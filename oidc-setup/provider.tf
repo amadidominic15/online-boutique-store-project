b02ef1cc3effb7fdb04037d76841f93998a9659e
Amadidominic15 <amadidominic15@gmail.com>
2025-10-15 10:19:46 +0100

    deploy oidc

--- oidc-setup/provider.tf
+++ oidc-setup/provider.tf
@@ -1,11 +1,12 @@
 terraform {
   backend "s3" {
-    region          = var.region
+    region          = "var.region"
     bucket          = "online-boutique-2025"
     key             = "githuboidc/terraform.tfstate"
     use_lockfile    = true
     }
-
+  
+  required_version = ">= 1.11.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -15,5 +16,5 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  region = "var.region"
 }
